cd569d837356d296d8c9edefaf0a18341d2fe3fc
Yash Shukla <shuklayash457@gmail.com>
2023-12-11 16:33:28 +0530

    eslint update

--- src/services/productService.js
+++ src/services/productService.js
@@ -10,7 +10,7 @@ export async function getProductList(searchTerm){
 export async function getProduct(id){
     const response = await fetch(`${process.env.REACT_APP_HOST}/444/products/${id}`);
     if(!response.ok){
-        throw { message: response.statusText, status: response.status };//eslint-disable-next-line
+        throw { message: response.statusText, status: response.status };//eslint-disable-line
     }
     const data = await response.json()
     return data;
