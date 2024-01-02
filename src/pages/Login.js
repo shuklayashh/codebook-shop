0daa1c85f2157aa5dee4feb2f3285c0d8ca8d39d
Yash Shukla <shuklayash457@gmail.com>
2024-01-03 00:24:37 +0530

    login js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
-import { useTitle } from "../hooks/useTitle";
 import { login } from "../services";
+import { useTitle } from "../hooks/useTitle";
+
 
 export const Login = () => {
   useTitle("Login");
