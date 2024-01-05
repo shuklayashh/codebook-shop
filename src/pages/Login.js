f16ba51ea81aa8f4a0bfb449baa2b156c9946ef5
Yash Shukla <shuklayash457@gmail.com>
2024-01-05 10:46:40 +0530

    update the project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 import { useTitle } from "../hooks/useTitle";
 import { login } from "../services";
-import { useTitle } from "../hooks/useTitle";
+
 
 
 
