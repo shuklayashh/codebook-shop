fe6e794fcace7ca302b70572e3c967568b90efd5
Yash Shukla <shuklayash457@gmail.com>
2024-01-05 11:24:55 +0530

    update project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -42,20 +42,7 @@ export const Login = () => {
   }
 
 
-  async function handleLoginGuest(){
-    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
-    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
-    try{
-      const authDetail={
-        email: email.current.value,
-        password: password.current.value
-      }
-      const data = await login(authDetail)
-      data.accessToken ? navigate("/products") : toast.error(data);
-    }catch(error){
-      toast.error(error.message, {closeButton: true, position: "bottom-center"});
-    }
-    }
+  
     
   return (
     <main>
