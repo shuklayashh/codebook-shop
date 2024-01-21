dbf11ae61427de7b27dd4bf895d211d919e7f4a4
Yash Shukla <shuklayash457@gmail.com>
2024-01-21 23:14:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Footer, Header } from './components';
 
 function App() {
   return (
-    <div className="App dark: dark:bg-slate-800">
+    <div className="App dark:bg-dark">
       <Header />
       <AllRoutes />
       <Footer />
