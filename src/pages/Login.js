2311d072b738724c63b88ce322c774f05d278f80
Yash Shukla <shuklayash457@gmail.com>
2024-01-24 12:21:38 +0530

    removel

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTitle } from "../hooks/useTitle";
 import { toast } from 'react-toastify';
 import { login } from "../services";
-import { useTitle } from "../hooks/useTitle";
+
 
 
 
@@ -26,7 +27,7 @@ export const Login = () => {
       toast.error(error.message, {closeButton: true, position: "bottom-center"});
     }
   }
-  async function handleLoginGuest(event) {
+  /*async function handleLoginGuest(event) {
     event.preventDefault();
   
     email.current.value=process.env.REACT_APP_GUEST_LOGIN;
@@ -41,7 +42,7 @@ export const Login = () => {
     } catch(error){
       toast.error(error.message, {closeButton: true, position: "bottom-center"});
     }
-  }
+  }*/
   
     
   return (
@@ -61,7 +62,7 @@ export const Login = () => {
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
           </form>
 
-         <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
+       { /* <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>*/}
     </main>
   )
 }
